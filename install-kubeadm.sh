e844816b0b7f7f5a3016fa85009c3d4ea9f86773
gomesuit <gomesuit@gmail.com>
2017-05-04 02:25:58 +0900

    fix

--- install-kubeadm.sh
+++ install-kubeadm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -s /vagrant/settings/kubernetes.repo
+ln -s /vagrant/settings/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
 yum install -y kubeadm
 systemctl enable kubelet
 setenforce 0
