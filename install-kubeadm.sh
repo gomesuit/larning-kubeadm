1adcec601df93d338ad33769c0b2789b38017700
gomesuit <gomesuit@gmail.com>
2017-05-09 04:06:26 +0900

    save

--- install-kubeadm.sh
+++ install-kubeadm.sh
@@ -3,6 +3,6 @@
 ln -s /vagrant/settings/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
 ln -s /vagrant/settings/k8s.conf /etc/sysctl.d/k8s.conf
 setenforce 0
-yum install -y docker kubelet kubeadm kubectl kubernetes-cni
+yum install -y --enablerepo=kubernetes docker kubelet kubeadm kubectl kubernetes-cni
 systemctl enable docker && systemctl start docker
 systemctl enable kubelet && systemctl start kubelet
