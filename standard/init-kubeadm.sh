89e0ec7bc967daa0eaf775cd3061c2f968c90c94
gomesuit <gomesuit@gmail.com>
2017-10-08 02:03:34 +0900

    fail-swap-on=false

--- standard/init-kubeadm.sh
+++ standard/init-kubeadm.sh
@@ -3,6 +3,7 @@ set -ex
 
 echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
 kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10 --pod-network-cidr 10.244.0.0/16
+#kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10 --pod-network-cidr 10.244.0.0/16 --kubernetes-version 1.7.7
 #kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10
 JOIN_TOKEN=$(kubeadm token list | grep 'default' | awk '{print $1}')
 
--- standard/install-kubeadm.sh
+++ standard/install-kubeadm.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -ex
 
-ln -s /vagrant/settings/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
-ln -s /vagrant/settings/k8s.conf /etc/sysctl.d/k8s.conf
+ln -sf /vagrant/settings/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
+ln -sf /vagrant/settings/k8s.conf /etc/sysctl.d/k8s.conf
 setenforce 0
 yum install -y --enablerepo=kubernetes docker kubelet kubeadm kubectl kubernetes-cni
+
+echo 'Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"' >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
 systemctl enable docker && systemctl start docker
 systemctl enable kubelet && systemctl start kubelet
