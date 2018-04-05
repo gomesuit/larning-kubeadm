1b5abd4d5b0c91eafd999c2ce43c166efe6e10f6
gomesuit <gomesuit@gmail.com>
2018-04-06 03:49:40 +0900

    save

--- standard/init-kubeadm.sh
+++ standard/init-kubeadm.sh
@@ -4,10 +4,15 @@ set -ex
 # https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
 
 kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10 --pod-network-cidr 10.244.0.0/16
-#kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10 --pod-network-cidr 10.244.0.0/16 --kubernetes-version 1.7.7
+# kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10 --pod-network-cidr 10.244.0.0/16 --kubernetes-version 1.7.7
+
 JOIN_TOKEN=$(kubeadm token list | grep 'default' | awk '{print $1}')
 
 echo $JOIN_TOKEN
 echo $JOIN_TOKEN > /vagrant/.token
 
-kubectl --kubeconfig /etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+mkdir -p ~/.kube
+cp /etc/kubernetes/admin.conf ~/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+# kubectl --kubeconfig /etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
--- standard/join-kubeadm.sh
+++ standard/join-kubeadm.sh
@@ -9,5 +9,4 @@ fi
 MASTER_IP=$1
 JOIN_TOKEN=$(cat /vagrant/.token)
 
-echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
 kubeadm join --skip-preflight-checks --token "$JOIN_TOKEN" "$MASTER_IP:6443"
