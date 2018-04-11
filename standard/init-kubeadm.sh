6d4795e2e9ce64a1b306099518ba995be7f1cfa8
gomesuit <gomesuit>
2018-04-12 05:42:24 +0900

    save

--- standard/init-kubeadm.sh
+++ standard/init-kubeadm.sh
@@ -4,7 +4,7 @@ set -ex
 # https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
 
 sudo kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10 --pod-network-cidr 10.244.0.0/16
-# sudo kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10 --pod-network-cidr 10.244.0.0/16 --kubernetes-version 1.7.7
+# sudo kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.33.10 --pod-network-cidr 10.244.0.0/16 --kubernetes-version 1.9.0
 
 sudo mkdir -p /root/.kube
 sudo cp /etc/kubernetes/admin.conf /root/.kube/config
