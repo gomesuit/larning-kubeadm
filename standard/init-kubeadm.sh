6856246ecfaaaff3f8c3320f4f7d899ee43235e6
gomesuit <gomesuit>
2018-04-12 05:43:39 +0900

    save

--- standard/init-kubeadm.sh
+++ standard/init-kubeadm.sh
@@ -9,5 +9,6 @@ sudo kubeadm init --skip-preflight-checks --apiserver-advertise-address 192.168.
 sudo mkdir -p /root/.kube
 sudo cp /etc/kubernetes/admin.conf /root/.kube/config
 
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+# sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+sudo kubectl apply -f https://raw.githubusercontent.com/gomesuit/larning-kubeadm/master/standard/vagrant-kube-flannel.yml
 # sudo kubectl --kubeconfig /etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
