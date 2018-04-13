0f13d6bf237b2a2b5b1a623cc7bdeff258309fa9
gomesuit <gomesuit>
2018-04-14 01:18:17 +0900

    動いた

--- standard/sample-manifest/job/load-queue.sh
+++ standard/sample-manifest/job/load-queue.sh
@@ -1,10 +1,9 @@
-#/bin/bash -e
+#!/bin/bash -e
 
 # 'keygen'というキューを作成
-curl -X PUT localhost:8080/memq/server/queue/keygen
+curl -X PUT localhost:8080/memq/server/queues/keygen
 
 # サブタスク(work item)を100個作ってキューに入れる
 for i in work-item-{0..99}; do
-  curl -X PUT localhost:8080/memq/server/queue/keygen/enqueue \
-    -d "$i"
+  curl -X POST localhost:8080/memq/server/queues/keygen/enqueue -d "$i"
 done
